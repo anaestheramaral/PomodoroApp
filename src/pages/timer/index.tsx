da2afc832fc253350ddffde9cfa79fab07e0bfb5
anaestheramaral <anaestheramaral@gmail.com>
2020-12-12 22:27:55 +0100

    feat:timer working
    play/pause and reset working

--- src/pages/timer/index.tsx
+++ src/pages/timer/index.tsx
@@ -1,10 +1,49 @@
-import React from 'react';
-import { FaPlay, FaUndoAlt, FaStepForward } from 'react-icons/fa';
+/* eslint-disable no-else-return */
+/* eslint-disable react/jsx-one-expression-per-line */
+import React, { useEffect, useState } from 'react';
+import { FaPlay, FaUndoAlt, FaStepForward, FaPause } from 'react-icons/fa';
 import { Circle, Container, TimerContainer } from './style';
 import NavBar from '../../components/navbar';
 import Button from '../../components/button';
 
-const Timer: React.FC = () => {
+const TimerPomodoro: React.FC = () => {
+  // const [minutes, setMinutes] = useState(60);
+  const [seconds, setSeconds] = useState(25 * 60);
+  const [isRunning, setIsRunning] = useState(false);
+
+  // useEffect(() => {
+  //   if (isRunning) {
+  //     const id = setInterval(
+  //       // eslint-disable-next-line no-shadow
+  //       () => setSeconds(seconds => seconds - 1),
+  //       1000,
+  //     );
+  //     return () => clearInterval(id);
+  //   } else {}
+  // }, [isRunning]);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      if (isRunning) {
+        setSeconds(s => s - 1);
+      }
+    }, 1000);
+    return () => {
+      clearInterval(id);
+    };
+  }, [isRunning]);
+
+  function startTimer() {
+    setIsRunning(true);
+  }
+  function pauseTimer() {
+    setIsRunning(false);
+  }
+  function resetTimer() {
+    setIsRunning(false);
+    setSeconds(25 * 60);
+  }
+
   return (
     <>
       <Container>
@@ -12,17 +51,23 @@ const Timer: React.FC = () => {
         <TimerContainer>
           <Circle>
             <div>
-              <strong>25:00</strong>
+              <strong>
+                {`${Math.floor(seconds / 60)}:${`00${seconds % 60}`.slice(-2)}`}
+              </strong>
               <p>focus</p>
             </div>
           </Circle>
           <div>
-            <Button type="button">
+            <Button type="button" onClick={resetTimer}>
               <FaUndoAlt />
             </Button>
 
-            <Button type="button" className="Play">
-              <FaPlay />
+            <Button
+              type="button"
+              onClick={isRunning ? pauseTimer : startTimer}
+              className="Play"
+            >
+              {isRunning ? <FaPause /> : <FaPlay />}
             </Button>
 
             <Button type="button">
@@ -35,4 +80,4 @@ const Timer: React.FC = () => {
   );
 };
 
-export default Timer;
+export default TimerPomodoro;
