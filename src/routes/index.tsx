38eb90f2b0253e9b0649ffaf9b662adc04d22840
anaestheramaral <anaestheramaral@gmail.com>
2021-01-02 23:59:08 +0100

    chore: deleted not used files

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,19 +4,17 @@ import { Switch, Route } from 'react-router-dom';
 import Timer from '../pages/timer/index';
 import Settings from '../pages/settings/index';
 import About from '../pages/about';
-// import Navbar from '../components/navbar';
 import Menu from '../components/menu/index';
-import { Container } from './styles';
+// import { Container } from './styles';
 
 const Routes: React.FC = () => (
-  <Container>
-    {/* <Navbar /> */}
+  <>
     <Menu />
     <Switch>
       <Route path="/" exact component={Timer} />
       <Route path="/settings" component={Settings} />
       <Route path="/about" component={About} />
     </Switch>
-  </Container>
+  </>
 );
 export default Routes;
